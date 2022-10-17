9cd3cbcb57205abdf986f02bac7d9e8dbc64f184
enc-rypto <64603408+enc-rypto@users.noreply.github.com>
2022-10-17 14:02:35 +0530

    created python solution for palindrome problem

---
+++ #9_palindrom_number.py
@@ -0,0 +1,9 @@
+# leetcode question : palindrome numnber
+#   problem link : https://leetcode.com/problems/palindrome-number/
+ 
+def isPalindrome(self, x):
+    if x < 0 or (x > 0 and x % 10 == 0): return False
+    half = 0
+    while x > half:
+        half, x = half * 10 + x % 10, x / 10
+    return x in (half, half / 10)
