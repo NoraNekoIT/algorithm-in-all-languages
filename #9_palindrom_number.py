16bd2e0320b5c192035103a1228243fef3c2ce82
enc-rypto <64603408+enc-rypto@users.noreply.github.com>
2022-10-18 08:54:44 +0530

    Update #9_palindrom_number.py

--- #9_palindrom_number.py
+++ #9_palindrom_number.py
@@ -7,3 +7,11 @@ def isPalindrome(self, x):
     while x > half:
         half, x = half * 10 + x % 10, x / 10
     return x in (half, half / 10)
+
+strin = str(input())
+ans = isPalindrome(strin)
+ 
+if ans:
+    print("Yes")
+else:
+    print("No"
